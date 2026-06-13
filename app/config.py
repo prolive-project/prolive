55e2478bafb2f892ddd491ab122d380f1432af31
prolive-project <prolive.project@outlook.es>
2026-06-13 17:35:49 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,13 @@
 import os
 
-class Config:
-    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # Ir al nivel superior
-    SECRET_KEY = os.getenv('SECRET_KEY', 'super_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'database.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+uri = os.getenv("DATABASE_URL")
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'database.db')
+
+SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
